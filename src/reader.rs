89d24ae0cc111b2bfd25902979242900d79f23e6
6br <tfdvrt@gmail.com>
2020-06-04 01:13:34 +0900

    chunk eprint

--- src/reader.rs
+++ src/reader.rs
@@ -271,6 +271,7 @@ impl<R: Read + Seek> IndexedReader<R> {
         let chunks = self
             .index
             .fetch_chunks(region.ref_id(), region.start(), region.end());
+        eprintln!("{:?}", chunks);
 
         self.reader.set_chunks(chunks);
 
