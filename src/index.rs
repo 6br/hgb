d01d01107170958e3ad5d490b38dfb33720b3a37
6br <tfdvrt@gmail.com>
2020-06-15 11:37:02 +0900

    Remove comment

--- src/index.rs
+++ src/index.rs
@@ -315,9 +315,9 @@ impl Bin {
     pub fn to_stream<W: Write>(&self, stream: &mut W) -> Result<()> {
         stream.write_u32::<LittleEndian>(self.bin_id)?;
         let chunks = self.merge_chunks();
-        if chunks.len() > 1 {
-            debug!("{:?} {:?}", self.chunks(), chunks);
-        }
+        /*if chunks.len() > 1 {
+            eprintln!("{:?} {:?}", self.chunks(), chunks);
+        }*/
         let n_chunks = chunks.len() as i32;
         stream.write_i32::<LittleEndian>(n_chunks)?;
 
