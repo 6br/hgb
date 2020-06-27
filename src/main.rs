0203a5784387dda9f24a5429a2314d5407bfb52c
6br <tfdvrt@gmail.com>
2020-06-27 11:05:42 +0900

    Hide eprintln

--- src/main.rs
+++ src/main.rs
@@ -828,7 +828,7 @@ fn vis_query(matches: &ArgMatches, threads: u16) -> Result<(), Box<dyn std::erro
                                };*/
                             //let index =
                             index_list.push(index + last_prev_index);
-                            eprintln!("{:?}", packing);
+                            // eprintln!("{:?}", packing);
                             //(index, (k.0, k.1))
                         }); //.collect::<Vec<(usize, (u64, Record))>>()
                             // compressed_list.push(prev_index);
