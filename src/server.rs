4e5d5bbb8c054bda25595e2a2e67a7a8da519f68
6br <tfdvrt@gmail.com>
2020-07-22 00:18:51 +0900

    Fix commands

--- src/server.rs
+++ src/server.rs
@@ -169,8 +169,11 @@ fn id_to_range<'a>(range: &StringRegion, args: &Vec<String>, zoom: u64, path: u6
     } else {
         vec!["-Y".to_string(), (freq_y / (max_zoom-zoom)).to_string(), "-y".to_string(), (y / (max_zoom-zoom)).to_string(), "-X".to_string(), (scalex_default / (max_zoom-zoom)).to_string(), "-n".to_string(), "-I".to_string()]
     };
-    b.extend(args.clone());
-    b.insert(0, "vis".to_string());
+    let args = args.clone();
+    args.extend(b); //b.extend(args.clone());
+    args.remove(0);
+    eprintln!("{}", args);
+    //b.insert(0, "vis".to_string());
     let matches = app.get_matches_from(b);
     // let args: Vec<String> = args.into_iter().chain(b.into_iter()).collect(); 
     (matches, StringRegion::new(&format!("{}:{}-{}", range.path, criteria * path + range.start, range.start + criteria * (path + 1)).to_string()).unwrap())
