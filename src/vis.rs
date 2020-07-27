792ff39250dd78c21b2396d026a1c9373e5c33e0
6br <tfdvrt@gmail.com>
2020-07-27 10:12:45 +0900

    Debug

--- src/vis.rs
+++ src/vis.rs
@@ -161,6 +161,10 @@ where
                 .x_label_formatter(&|x| format!("{:.3}", x))
                 .draw()?;
             let color = Palette99::pick(idx); // BLUE
+            eprintln!("{} {:?}", y_max, values
+            .iter()
+            .filter(|t| t.0 >= range.start() && t.0 < range.end())
+            .map(|t| *t));
             chart
                 .draw_series(
                     Histogram::vertical(&chart).style(color.filled()).data(
