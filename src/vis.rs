ebd8cc7100a96fc49bbd97a04b0693a601ecc8f6
6br <tfdvrt@gmail.com>
2020-07-27 11:08:41 +0900

    Fix visiblity ratio

--- src/vis.rs
+++ src/vis.rs
@@ -434,7 +434,7 @@ where
                                     (start, prev_index + key * 2 + axis_count + 1),
                                     (end, prev_index + key * 2 + axis_count + 1),
                                 ],
-                                stroke.stroke_width(y / 3 * 4),
+                                stroke.stroke_width(y / 4 * 3),
                             ))
                             .unwrap()
                             .label(format!("{}", record.name().unwrap_or(&"")))
