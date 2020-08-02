8a3a1da325258996bdd8be65c22fcf6399ae7e90
6br <tfdvrt@gmail.com>
2020-08-02 13:31:37 +0900

    Update colorscheme

--- src/vis.rs
+++ src/vis.rs
@@ -31,6 +31,7 @@ macro_rules! predefined_color {
     }*/
 }
 
+/*
 predefined_color!(
     NEG_COL,
     150,
@@ -54,6 +55,39 @@ predefined_color!(A_COL, 0, 200, 0, "The predefined A color");
 predefined_color!(C_COL, 0, 0, 200, "The predefined C color");
 predefined_color!(T_COL, 255, 0, 40, "The predefined T color");
 predefined_color!(G_COL, 209, 113, 5, "The predefined G color");
+*/
+
+// Now I adapt nordtheme. https://www.nordtheme.com/
+predefined_color!(
+    NEG_COL,
+    0x5e,
+    0x81,
+    0xac,
+    /*0.75,*/
+    "The predefined negative-str color"
+);
+predefined_color!(
+    POS_COL,
+    0xbf,
+    0x61,
+    0x6a,
+    /*0.75,*/
+    "The predefined positive-str color"
+);
+predefined_color!(INS_COL, 0xb4, 0x8e, 0xad, "The predefined insertion color");
+predefined_color!(
+    SPL_COL,
+    0x8f,
+    0xbc,
+    0xbb,
+    "The predefined split-alignment color"
+);
+predefined_color!(N_COL, 80, 80, 80, "The predefined N color");
+predefined_color!(A_COL, 0, 200, 0, "The predefined A color");
+predefined_color!(C_COL, 0, 0, 200, "The predefined C color");
+predefined_color!(T_COL, 255, 0, 40, "The predefined T color");
+predefined_color!(G_COL, 209, 113, 5, "The predefined G color");
+
 //RGBColor();
 
 //newtype RecordIter<'a> = std::slice::Iter<'a, (u64, Record)>;
