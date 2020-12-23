fc3d225873885aa6ba23e8779ac134fed3110583
6br <tfdvrt@gmail.com>
2020-12-23 17:23:08 +0900

    Add image wrapper

--- image_wrapper.sh
+++ image_wrapper.sh
@@ -14,7 +14,7 @@ $CMD -t1 vis -_ 1000000 $ARGS -o dnd/0.png 2>/dev/null &
 
 for i in `seq 1 ${READ_MAX}`
 do
-  $CMD -t1 vis -_ $((i-1)) $ARGS -o dnd/$i.png -* 2>/dev/null &
+  $CMD -t1 vis -_ $((i-1)) $ARGS -o dnd/$i.png -* 2>/dev/null
 done
 
-$CMD -t1 vis $ARGS -P -A -o dnd/$((READ_MAX+2)).png 2>/dev/null &
+$CMD -t1 vis $ARGS -P -A -o dnd/$((READ_MAX+2)).png 2>/dev/null
