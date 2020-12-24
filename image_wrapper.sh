c843778eeaedfb191e82388136f80b1f62a022f8
6br <tfdvrt@gmail.com>
2020-12-24 14:03:16 +0900

    Add image wrapper

--- image_wrapper.sh
+++ image_wrapper.sh
@@ -10,7 +10,7 @@ fi
 shift
 ARGS=$@
 
-$CMD -t1 vis -_ 1000000 $ARGS -o dnd/0.png 2>/dev/null &
+$CMD -t1 vis -_ 1000000 $ARGS -o dnd/0.png 2>/dev/null
 
 for i in `seq 1 ${READ_MAX}`
 do
@@ -18,3 +18,20 @@ do
 done
 
 $CMD -t1 vis $ARGS -P -A -o dnd/$((READ_MAX+2)).png 2>/dev/null
+
+if [ ! -e "./build.zip" ]; then
+  wget https://github.com/6br/interactive-hgb/releases/download/v0.1.0/build.zip 
+  unzip build.zip
+fi
+
+mv dnd build/
+cd build
+
+ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
+if [ $ret -eq 0 ]; then
+    #echo "we require python version <3"
+    python -m http.server 8000
+else
+    #echo "python version is <3"
+    python -m SimpleHTTPServer 8000
+fi
